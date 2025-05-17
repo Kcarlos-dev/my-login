42fd0e54f9b7d141f5127375a678fb530626b2c1
Kcarlos <ca850075@gmail.com>
2025-05-17 13:08:57 -0300

    Criando função de cadastro de usuario

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const user = require("./models/User")
+
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -14,8 +16,21 @@ mongoose.connect("mongodb://172.17.0.2:27017/guiapics", {
     console.log(err)
 })
 
+let User = mongoose.model("User",user)
+
 app.get("/", (req, res) => {
     res.json({})
+})
+
+app.post("/user", async(req,res)=>{
+    try {
+        const newUser = new User({name: req.body.name, email: req.body.email, password: req.body.password})
+        await newUser.save()
+        res.json({email:req.body.email})
+        
+    } catch (error) {
+        res.sendStatus(500)
+    }
 })
 
 module.exports = app
