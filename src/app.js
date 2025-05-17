4aa5f21f64395fcac4dab19210d9f78f7171184c
Kcarlos <ca850075@gmail.com>
2025-05-17 12:21:07 -0300

    Criando conexão com o banco

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get("/",(req,res)=>{
+mongoose.connect("mongodb://172.17.0.2:27017/guiapics", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Conectado com banco")
+}).catch((err) => {
+    console.log(err)
+})
+
+app.get("/", (req, res) => {
     res.json({})
 })
 
